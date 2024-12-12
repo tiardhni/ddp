e215728b7e49358addf0a92ccb658a8eb2d8770f
tiardhni <tiaraaa679@gmail.com>
2024-12-12 19:17:42 +0700

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,12 @@
+class Animal: 
+    def __init__(self, nama, makanan, hidup, berkembang_biak):
+        self.nama = nama
+        self.makanan = makanan
+        self.hidup = hidup
+        self.berkembang_biak = berkembang_biak
+    def cetak(self):
+        print("Nama \t\t: ", self.nama,
+               "\nmakanan \t: ", self.makanan,
+               "\nhidup \t\t: ", self.hidup,
+               "\nberkembang biak : ", self.berkembang_biak
+               )
